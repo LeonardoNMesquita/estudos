be4ed5855ad2e43d0443d44c92b3f35b254de6c3
LeonardoNMesquita <leo_n_mesquita@hotmail.com>
2020-03-29 20:49:22 -0300

    Capturar nome e exibe. Exercicio Pronto.

--- estudoGit.java
+++ estudoGit.java
@@ -1,4 +1,8 @@
-
-public class estudoGit {
-
+import javax.swing.*;
+public class EntregaGit {
+public static void main (String [] args){
+System.out.println ("Entregando tarefas com o Git na linha de comando...");
+String nome = JOptionPane.showInputDialog("Qual seu nome?");
+JOptionPane.showMessageDialog(null, "Oi, " + nome);
+}
 }
